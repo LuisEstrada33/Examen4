7308c1b256f9d8f33afb75140be18abed3783f58
LuisEstrada33 <43280434+LuisEstrada33@users.noreply.github.com>
2018-11-23 16:47:47 -0800

    Add files via upload

---
+++ Examen4-Pt.1/Examen4-Pt.1/Grafo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen4_Pt._1
+{
+    class Graph
+    {
+        public int Vertices { get; set; } // Atributo vertice del grafo
+        readonly List<int>[] List; // Lista entera  se crea
+        public Graph(int value) // Constructor
+        {
+            Vertices = value; // Vertice se igual al valor agregado 
+            List = new List<int>[value]; // se agrega el valor del parametro
+            for (int i = 0; i < value; i++) // un ciclo for para estar creando una lista
+            {
+                List[i] = new List<int>();
+            }
+        }
+
+        public void Add(int Indice, int Valor) // Para agregar un valor a  la lista
+        {
+            List[Indice].Add(Valor); // agrega el valor que le des a la lista del indice
+        }
+
+
+
+        public void DFS(int Value, int Data) // Busca el camino al ultimo elemento
+        {
+            List<string> City = new List<string>();
+            if (Data == 1 || Data == 2)
+            {
+                City.Add("Boston");
+                City.Add("Chicago");
+                City.Add("Denver");
+                City.Add("LA");
+                City.Add("New York");
+                City.Add("San Francisco");
+                City.Add("Miami");
+                City.Add("Atlanta");
+            }
+            if (Data == 3)
+            {
+                City.Add("Atlanta");
+                City.Add("Chicago");
+                City.Add("San Francisco");
+                City.Add("Boston");
+                City.Add("Denver");
+                City.Add("LA");
+                City.Add("New York");
+                City.Add("Miami");
+            }
+            if (Data == 4)
+            {
+                City.Add("Denver");
+                City.Add("Chicago");
+                City.Add("New York");
+                City.Add("Atlanta");
+                City.Add("San Francisco");
+                City.Add("Boston");
+                City.Add("LA");
+                City.Add("Miami");
+            }
+
+            bool[] Visita = new bool[Vertices]; // crea una arreglo boleano de tamaño atributo vertices
+            Stack<int> Stack = new Stack<int>(); // crea una pila 
+            Visita[Value] = true; // el valor del arreglo con el parametro es igual a true
+            Stack.Push(Value); // agrega el valor parametro a la pila
+            Console.SetCursorPosition(0, 8);
+            Console.WriteLine("La ruta es: ");     // EL camino al ultimo elemento es       
+            while (Stack.Count != 0) // mientras la pila no este vacio
+            {
+                Value = Stack.Pop(); // Valor se igual al ultmo elemento de la pila (lo saca)
+                Console.Write("{0}-> ", City[Value]); // agrega el valor                
+                foreach (int item in List[Value]) // por cada elemento entero en la lista de [Valor dado]
+                {
+                    if (!Visita[item]) // Si visita en la posicion item es diferente
+                    {
+                        Visita[item] = true; // esa posicion es igual a true
+                        Stack.Push(item); // agrega el elemento item a la pila
+                    }
+                }
+            }
+            Console.Write("Fin de la ruta");
+        }
+    }
+}
---
+++ Examen4-Pt.1/Examen4-Pt.1/Operaciones.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen4_Pt._1
+{
+    class Operaciones
+    {
+        public void Graph()
+        {
+            Select:
+            Console.Clear();
+            Console.WriteLine("Seleccione la Ruta que desea mirar:\n1) Boston->LA\n2)NewYork->San Francisco\n3)Atlanta->San Francisco\n4)Denver->NewYork\n5)Salir");
+            int Data = int.Parse(Console.ReadLine());
+            if (Data == 1)
+            {
+                Graph Graph = new Graph(5);
+                Graph.Add(0, 1);
+                Graph.Add(1, 2);
+                Graph.Add(2, 3);
+                Graph.DFS(0, 1);
+                Console.WriteLine("\n\nPresione para continuar...");
+                Console.ReadKey();
+                goto Select;
+            }
+
+            if (Data == 2)
+            {
+                Graph Graph = new Graph(7);
+                Graph.Add(0, 1);
+                Graph.Add(1, 2);
+                Graph.Add(2, 3);
+                Graph.Add(4, 5);
+                Graph.DFS(4, 2);
+                Console.WriteLine("\n\nPresione para continuar...");
+                Console.ReadKey();
+                goto Select;
+            }
+
+            if (Data == 3)
+            {
+                Graph Graph = new Graph(4);
+                Graph.Add(0, 1);
+                Graph.Add(1, 2);
+                Graph.DFS(0, 3);
+                Console.WriteLine("\n\nPresione para continuar...");
+                Console.ReadKey();
+                goto Select;
+            }
+
+            if (Data == 4)
+            {
+                Graph Graph = new Graph(4);
+                Graph.Add(0, 1);
+                Graph.Add(1, 2);
+                Graph.DFS(0, 4);
+                Console.WriteLine("\n\nPresione para continuar...");
+                Console.ReadKey();
+                goto Select;
+            }
+            if (Data == 5)
+            {
+                Console.Clear();
+                Console.WriteLine("Presione una tecla para salir");
+                Console.ReadKey();
+            }
+        }
+    }
+}
